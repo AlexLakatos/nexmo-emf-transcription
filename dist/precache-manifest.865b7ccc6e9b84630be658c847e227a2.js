b95d04a675a605e4e1994245a23eae89e5a5a749
Alex Lakatos <alex.lakatos.dev@gmail.com>
2018-08-27 15:13:41 +0100

    adding build files

---
+++ dist/precache-manifest.865b7ccc6e9b84630be658c847e227a2.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "6c8d859fde358b898ecd",
+    "url": "/nexmo-emf-transcription/js/chunk-7db3.77535a97.js"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/nexmo-emf-transcription/robots.txt"
+  },
+  {
+    "revision": "6054c19f7ff16b44322d",
+    "url": "/nexmo-emf-transcription/js/chunk-vendors.0cb28c90.js"
+  },
+  {
+    "revision": "6d07a7ad30924b8b9ced",
+    "url": "/nexmo-emf-transcription/js/chunk-7c73.ccb6f351.js"
+  },
+  {
+    "revision": "0630d312fbe0ff778b33",
+    "url": "/nexmo-emf-transcription/js/chunk-3075.0d9c82d5.js"
+  },
+  {
+    "revision": "805214876098fee2c82d",
+    "url": "/nexmo-emf-transcription/js/app.13b5160d.js"
+  },
+  {
+    "revision": "8db46b4e174ad7dc307985d6f38f2af7",
+    "url": "/nexmo-emf-transcription/index.html"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/nexmo-emf-transcription/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "cb353ae2fa5e3663d2aa556f708853e6",
+    "url": "/nexmo-emf-transcription/data/stages.json"
+  },
+  {
+    "revision": "6d07a7ad30924b8b9ced",
+    "url": "/nexmo-emf-transcription/css/chunk-7c73.de4cbaae.css"
+  },
+  {
+    "revision": "0630d312fbe0ff778b33",
+    "url": "/nexmo-emf-transcription/css/chunk-3075.4c642f45.css"
+  },
+  {
+    "revision": "805214876098fee2c82d",
+    "url": "/nexmo-emf-transcription/css/app.fda5efd9.css"
+  }
+];
---
+++ dist/service-worker.js
@@ -0,0 +1,29 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.4.1/workbox-sw.js");
+
+importScripts(
+  "/nexmo-emf-transcription/precache-manifest.865b7ccc6e9b84630be658c847e227a2.js"
+);
+
+workbox.core.setCacheNameDetails({prefix: "nexmo-emf-transcription"});
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
