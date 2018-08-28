e512380019b79fa4e1967322364174daed92a94a
Alex Lakatos <alex.lakatos.dev@gmail.com>
2018-08-28 17:47:25 +0100

    adding "logging"

--- server.js
+++ server.js
@@ -26,6 +26,9 @@ const userAcl = {
   },
 };
 
+console.log(process.env.APP_ID)
+console.log(process.env.PRIVATE_KEY)
+
 privateKey = Buffer.from(process.env.PRIVATE_KEY, 'utf-8');
 
 // endpoint that doesn't authenticate the user
