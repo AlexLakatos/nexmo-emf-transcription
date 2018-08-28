2efe5f32531fdbab8a8b25de5ecc3071be746db6
Alex Lakatos <alex.lakatos.dev@gmail.com>
2018-08-28 17:33:22 +0100

    adding Phil magic

--- server.js
+++ server.js
@@ -1,63 +1,66 @@
-require("dotenv").config({
-  path: __dirname + "/.env"
+require('dotenv').config({
+  path: `${__dirname}/.env`,
 });
 
-const Nexmo = require("nexmo");
-const express = require("express");
-const bodyParser = require("body-parser");
-const cors = require("cors");
+const Nexmo = require('nexmo');
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
 
 const app = express();
 app.use(bodyParser.json());
 app.use(cors());
-app.use(express.static(__dirname + "/dist"));
+app.use(express.static(`${__dirname}/dist`));
 
 const userAcl = {
   paths: {
-    "/v1/users/**": {},
-    "/v1/conversations/**": {},
-    "/v1/sessions/**": {},
-    "/v1/devices/**": {},
-    "/v1/image/**": {},
-    "/v3/media/**": {},
-    "/v1/applications/**": {},
-    "/v1/push/**": {},
-    "/v1/knocking/**": {}
-  }
+    '/v1/users/**': {},
+    '/v1/conversations/**': {},
+    '/v1/sessions/**': {},
+    '/v1/devices/**': {},
+    '/v1/image/**': {},
+    '/v3/media/**': {},
+    '/v1/applications/**': {},
+    '/v1/push/**': {},
+    '/v1/knocking/**': {},
+  },
 };
 
 let privateKey = process.env.PRIVATE_KEY;
+if (!require('fs').existsSync(privateKey)) {
+  privateKey = Buffer.from(privateKey, 'utf-8');
+}
 
 // endpoint that doesn't authenticate the user
 // it will simply return a JWT with every request
-app.get("/api/jwt/:user", (req, res) => {
+app.get('/api/jwt/:user', (req, res) => {
   const jwt = Nexmo.generateJwt(privateKey, {
     application_id: process.env.APP_ID,
     sub: req.params.user,
     exp: new Date().getTime() + 86400,
-    acl: userAcl
+    acl: userAcl,
   });
 
   res.json({ user_jwt: jwt });
 });
 
-app.get("/api/data/stages", (req, res) => {
+app.get('/api/data/stages', (req, res) => {
   res.json([
     {
-      name: "a",
-      stitch: "stagea",
-      id: "CON-1a7dd01f-120a-4dcd-a4d7-46ea9a46f1eb"
+      name: 'a',
+      stitch: 'stagea',
+      id: 'CON-1a7dd01f-120a-4dcd-a4d7-46ea9a46f1eb',
     },
     {
-      name: "b",
-      stitch: "stageb",
-      id: "CON-12bf3f5a-bb6e-4f2c-9f30-bd64afd227c4"
+      name: 'b',
+      stitch: 'stageb',
+      id: 'CON-12bf3f5a-bb6e-4f2c-9f30-bd64afd227c4',
     },
     {
-      name: "c",
-      stitch: "stagec",
-      id: "CON-e130a407-cfac-4ff6-b12a-04c8914ed97b"
-    }
+      name: 'c',
+      stitch: 'stagec',
+      id: 'CON-e130a407-cfac-4ff6-b12a-04c8914ed97b',
+    },
   ]);
 });
 
