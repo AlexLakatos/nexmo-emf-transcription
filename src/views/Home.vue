2dd87c253ffd675ea4dfc7cb6ba2c3e84815cfa7
Alex Lakatos <alex.lakatos.dev@gmail.com>
2018-08-28 16:26:34 +0100

    adding stage images

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="mdl-grid">
     <div class="mdl-cell mdl-cell--4-col mdl-cell--12-col-phone mdl-card mdl-shadow--2dp" v-for="stage in stages" :key="stage.id">
-      <div class="mdl-card__title mdl-card--expand">
+      <div :class="'mdl-card__title mdl-card--expand stage-' + stage.name">
         <h2 class="mdl-card__title-text">Stage {{ stage.name.toUpperCase() }}</h2>
       </div>
       <div class="mdl-card__actions mdl-card--border">
@@ -13,9 +13,24 @@
 </template>
 
 <style>
+.mdl-card__title.stage-a {
+  background-image: url("../assets/stagea.jpg");
+}
+.mdl-card__title.stage-b {
+  background-image: url("../assets/stageb.jpg");
+}
+.mdl-card__title.stage-c {
+  background-image: url("../assets/stagec.jpg");
+}
+
 .mdl-card__title {
-  background:
-    url('../assets/logo.png') bottom right no-repeat #46B6AC;
+  background-size: cover;
+}
+
+.mdl-card__title-text {
+  background-color: #009688;
+  padding: 5px;
+  color: #fff;
 }
 </style>
 
@@ -43,9 +58,7 @@ export default {
         // JSON responses are automatically parsed.
         this.stages = response.data;
       })
-      .catch((e) => {
-        this.error = e;
-      });
+      .catch(console.error);
   },
 };
 </script>
